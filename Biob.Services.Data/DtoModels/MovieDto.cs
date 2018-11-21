cdd8ee00a55ef25e0443270661fa5625a91ce050
BT <bjarke.treufeldt@gmail.com>
2018-11-21 11:05:25 +0100

    Small comment for discussion when we have time

--- Biob.Services.Data/DtoModels/MovieDto.cs
+++ Biob.Services.Data/DtoModels/MovieDto.cs
@@ -7,6 +7,10 @@ namespace Biob.Services.Data.DtoModels
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        //I'm having second thoughs about this "Length" as a string of xhours and xminutes, instead of an integer that represents seconds. 
+        //I know we discussed but i doubt that this is smartest.
+        //Wouldn't it make sense to always handle everything in the api the easiest and fastest way?
+        //I would think that by putting the formatting on the client's workload, we keep the server as clean and fast as possible
         public string Length { get; set; }
         public string Poster { get; set; }
         public string Producer { get; set; }
